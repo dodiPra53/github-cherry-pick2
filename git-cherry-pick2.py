f5e4ced0f1e046a864ef5b6b56a34f04032d7ec1
Dodi Agusri <dodi9802se@gmail.com>
2024-02-19 11:01:46 +0700

    commit pra rilis

---
+++ git-cherry-pick2.py
@@ -0,0 +1,14 @@
+from os import system, name
+n = 10
+num1 = 0
+num2 = 1
+next_number = num2 
+count = 1
+
+system('cls');
+while count <= n:
+	print(next_number, end=" ")
+	count += 1
+	num1, num2 = num2, next_number
+	next_number = num1 + num2
+print()
